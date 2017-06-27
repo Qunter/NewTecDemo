912d847f05bab2f4cceca666c0ba6d6a534f0958
Liu <504505485@qq.com>
2017-06-27 11:59:51 +0800

    finish

--- app/src/main/java/qunter/newtecdemo/ui/activity/MainActivity.java
+++ app/src/main/java/qunter/newtecdemo/ui/activity/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
-import com.youth.banner.Banner;
 
 import java.util.ArrayList;
 import java.util.List;
